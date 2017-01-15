26505fb5269cb69c48f2508dca5d12b4346e7cd2
tanbobo <tandingbo@163.com>
2017-01-16 05:46:14 +0800

    Initial commit

---
+++ src/main/java/com/tanbobo/dmps/DmpsApplication.java
@@ -0,0 +1,12 @@
+package com.tanbobo.dmps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmpsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/tanbobo/dmps/DmpsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tanbobo.dmps;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DmpsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
