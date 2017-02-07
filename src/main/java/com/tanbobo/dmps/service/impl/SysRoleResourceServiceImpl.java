dc6bd86af5268608ec85041aa2203708ea220422
tandingbo@163.com <TAN19881218bo>
2017-02-07 14:33:47 +0800

    rbac设计整理

--- src/main/java/com/tanbobo/dmps/service/impl/SysRoleResourceServiceImpl.java
+++ src/main/java/com/tanbobo/dmps/service/impl/SysRoleResourceServiceImpl.java
@@ -1,6 +1,5 @@
 package com.tanbobo.dmps.service.impl;
 
-import com.tanbobo.dmps.mapper.SysRoleResourceMapper;
 import com.tanbobo.dmps.service.SysRoleResourceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
